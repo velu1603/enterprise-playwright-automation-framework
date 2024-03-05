e9b2bb9470f16ea878b5247362940ed26a96fe68
Velan Chinnaiah <velu1603@gmail.com>
2024-03-05 16:39:23 +0000

    Initial commit

---
+++ src/pages/LoginPage.ts
@@ -0,0 +1,47 @@
+import { Page } from "@playwright/test";
+import HomePage from "./HomePage";
+import logger from "../utils/LoggerUtil";
+
+export default class LoginPage {
+  private readonly usernameInputSelector = "#username";
+  private readonly passwordInputSelector = "#password";
+  private readonly loginButtonSelector = "#Login";
+
+  constructor(private page: Page) {}
+
+  async quickLogin(username: string, password: string) {
+    await this.navigateToLoginPage();
+    await this.fillUsername(username);
+    await this.fillPassword(password);
+    return await this.clickLoginButton();
+  }
+
+  async navigateToLoginPage() {
+    await this.page.goto("/");
+    logger.info("Navigated to login.salesforce.com");
+  }
+
+  async fillUsername(username: string) {
+    await this.page.locator(this.usernameInputSelector).fill(username);
+    logger.info("Filled username");
+  }
+
+  async fillPassword(password: string) {
+    await this.page.locator(this.passwordInputSelector).fill(password);
+    logger.info("Filled pasword");
+  }
+
+  async clickLoginButton() {
+    await this.page
+      .locator(this.loginButtonSelector)
+      .click()
+      .catch((error) => {
+        logger.error(`Error clicking login button: ${error}`);
+        throw error; // rethrow the error if needed
+      })
+      .then(() => logger.info("Clicked login button"));
+
+    const homePage = new HomePage(this.page);
+    return homePage;
+  }
+}
